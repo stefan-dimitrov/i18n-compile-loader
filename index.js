c9e09347ec4934812858903c3df0433ab978b64b
stefan-dimitrov <stefan.dimitrov.009@gmail.com>
2017-03-24 10:45:05 +0200

    Basic translation.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 
-module.exports = function (source, sourceMap) {
-  console.log('source content:', source);
-  return source;
+const i18nCompile = require('i18n-compile');
+
+module.exports = function (source) {
+  var translations = i18nCompile.fromString(source);
+  return JSON.stringify(translations);
 };
